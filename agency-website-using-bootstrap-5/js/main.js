50037653d0b9179d7bb8154d38e06abc0346e6f6
Techu Mayur <techumayur@gmail.com>
2024-03-11 23:44:48 +0530

    agency website responsive fixed

--- agency-website-using-bootstrap-5/js/main.js
+++ agency-website-using-bootstrap-5/js/main.js
@@ -1,32 +1,41 @@
- (function ($) {
+(function ($) {
     "use strict";
 
 
     var $body = $("body"),
-            $window = $(window);  
+            $window = $(window);
 
+    /*============================= Navigation Section ==============================*/
+
+    $window.on('scroll' ,function () {
+        if ($(".navbar").offset().top > 25) {
+            $(".navbar-default").addClass("small");
+        } else {
+            $(".navbar-default").removeClass("small");
+        }
+    });
 
     /*============================= Smoothscroll js ==============================*/
     
-     $('.navbar-default').on('click', 'a', function (event) {
+    $('.navbar-default').on('click', 'a', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 75
         }, 1000);
         event.preventDefault();
     });
- 
+
     /*====================================== jquery scroll spy ========================================*/
-     
-     $body.scrollspy({
+    
+    $body.scrollspy({
         target: ".navbar-collapse",
         offset: 76
 
-    }); 
- 
+    });
+
     /*====================================== Fancybox ========================================*/
 
-      $('.fancybox-buttons').fancybox({
+    $('.fancybox-buttons').fancybox({
         openEffect: 'fade',
         closeEffect: 'fade',
         prevEffect: 'fade',
@@ -40,15 +49,15 @@
             },
             buttons: {}
         }
-    }); 
- 
+    });
+
     /*====================================== Fancybox-script ========================================*/
 
-    $(".fancybox").fancybox();  
+    $(".fancybox").fancybox();
 
     /*====================================== Mixit-up ========================================*/
 
-     var filterList = {
+    var filterList = {
         init: function () {
 
             // MixItUp plugin
@@ -71,29 +80,43 @@
     filterList.init();
 
     /*====================================== Parallex ========================================*/
- 
-    $('.parallax').scrolly({bgParallax: true}); 
+
+    $('.parallax').scrolly({bgParallax: true});
 
 
 })(jQuery);
- 
+
 
 /*============================= Preload==============================*/
 
- $(window).on('load' ,function () {
+$(window).on('load' ,function () {
     "use strict";
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");
     ;
-}); 
+});
 
 
+/*======================================counter-up ========================================*/
+
+$('.Count').each(function () {
+    "use strict";
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 10000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
 
 
 /*====================================== COLOR SCHEMES ========================================*/
 
 
- $(".option").on("click", function () {
+$(".option").on("click", function () {
     $(".box").toggleClass("open");
 });
 
@@ -122,5 +145,4 @@ $(".default").on("click", function () {
 });
 $(".style5").on("click", function () {
     $("#color").attr("href", "css/color1/style5.css");
-}); 
- 
+});
